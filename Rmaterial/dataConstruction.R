b2a26bcba788f631f763c3db9b32c3fb736dbd7a
jmonlong <jean.monlong@mail.mcgill.ca>
2013-10-20 16:22:26 -0400

    Preparing data, not finished yet

--- Rmaterial/dataConstruction.R
+++ Rmaterial/dataConstruction.R
@@ -4,3 +4,14 @@ rownames(mat) = paste("row",1:nrow(mat),sep="")
 colnames(mat) = paste("col",1:ncol(mat),sep="")
 
 save(mat,file="dataToPlayWith.Rdata")
+
+## Data to plot
+nb.samp=100
+nb.genes = 20000
+ge.mean = rpois(nb.genes,1)
+ge.mean[333] = 2
+mat.ge = matrix(rnorm(nb.samp*nb.genes,ge.mean,sqrt(ge.mean)),nb.genes,nb.samp)
+rownames(mat.ge) = paste("gene",1:nb.genes,sep="")
+colnames(mat.ge) = paste("sample",1:nb.samp,sep="")
+mat.ge[333,] = mat.ge[666,] + rnorm(nb.samp,0,sqrt(ge.mean[333])/5)
+
--- Rmaterial/solutions.R
+++ Rmaterial/solutions.R
@@ -17,6 +17,72 @@ max(mat[,3])
 max(mat[,4])
 colnames(mat)[1] ## If the highest max was found for mat[,1]
 
+## Functions
+power <- function(a,b){
+    return(exp(b*log(a)))
+}
+power(2,2)
+power(2,6)
+
+minMaxAve <- function(v){
+    return(mean(c(min(v),max(v))))
+}
+minMaxAve(0:10)
+minMaxAve(c(1,20:30))
+
+## Function with condition
+rem3 <- function(v){
+    return(v[which(v>3)])
+}
+rem3(1:10)
+rem3(4:10)
+rem3(1:3)
+
+remTh <- function(v,th=3){
+    return(v[which(v>th)])
+}
+remTh(1:10,3)
+remTh(1:10,9)
+
+## Function with condition and if/else
+classify <- function(v,low.th=3,high.th=7){
+    v.med = median(v)
+    if(v.med < low.th){
+        return("small")
+    } else if(v.med > high.th){
+        return("high")
+    } else {
+        return("medium")
+    }
+}
+classify(1:3)
+classify(1:10)
+classify(7:10)
+classify(1:20)
+
+## Function: apply vs loop
+meanCols.apply <- function(mat){
+    return(apply(mat,2,mean))
+}
+meanCols.loop <- function(mat){
+    res = rep(NA,ncol(mat))
+    for(i in 1:ncol(mat)){
+        res[i] = mean(mat[,i])
+    }
+    return(res)
+}
+meanCols.apply(matrix(1:12,3,4))
+meanCols.loop(matrix(1:12,3,4))
+
+## Plots
+hist(apply(mat.ge,1,mean),xlab="gene expression",ylab="number of genes",main="Average gene expression across the samples")
+abline(v=mean(ge.mean),lty=2)
+
+plot(mat.ge["gene333",],mat.ge["gene666",],xlab="gene333",ylab="gene666",main="Correlated genes")
+lines(mat.ge["gene333",],mat.ge["gene667",],type="p",col=2,pch=2)
+
+cor.ge = cor(t(mat.ge))
+
 ## One-liner quizz
 mean(apply(mat,2,mean)>0)
 colnames(mat)[which.max(apply(mat,2,max))]
